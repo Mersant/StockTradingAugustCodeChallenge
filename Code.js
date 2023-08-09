294ea083285fef442571639167a160922333125b
Mersant <Dlowry256@outlook.com>
2023-08-09 17:25:30 -0400

    Update Code.js

--- Code.js
+++ Code.js
@@ -1,7 +1,7 @@
 // Record was 53ms (1) or top 99.90%, Avg. across 10 runs was 69.2 or top ~93%
 // (1) https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/1016975770/
 var maxProfit = function(prices) {
-    var highestProfit = prices[1] - prices[0] >>> 0, // Force profit to unsigned int so that 0 will be returned (no trades) if highest profit is negative
+    var highestProfit = prices[1] > prices[0] >>> 0, // Force profit to unsigned int so that 0 will be returned (no trades) if highest profit is negative
         stockBuyPrice = prices[0];
 
     for(var i = 1; i < prices.length; i++) {
